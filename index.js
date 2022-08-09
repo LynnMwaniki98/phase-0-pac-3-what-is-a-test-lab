2589cf26ce69d97e7d2aeba03e7d9356e1a1bfaa
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-09 15:49:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const name = "Susan";
-const height = 39;
+const height = 35;
 const message = `${name} is ${height} inches tall`;
 (height <= 40);
 module.exports = {name, height, message};
+console.log(message);
